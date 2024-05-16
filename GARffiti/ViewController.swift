92e16b580f90dda139d82243223f167f59ba5b40
Paul Michael <paullmich28@gmail.com>
2024-05-16 11:36:39 +0700

    Color picker

--- GARffiti/ViewController.swift
+++ GARffiti/ViewController.swift
@@ -13,13 +13,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
     var exampleArray = [SCNNode]()
+    @IBOutlet weak var colorWheel: UIColorWell!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
-        sceneView.autoenablesDefaultLighting = true
         sceneView.delegate = self
     }
     
@@ -48,60 +48,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
             
             if let hitResult = results.first {
-                print("Plane clicked.")
-                let crownScene = SCNScene(named: "art.scnassets/crown.scn")!
+//                print("Plane clicked.")
+                let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.005))
+                let sphereMaterial = SCNMaterial()
+                sphereMaterial.diffuse.contents = colorWheel.selectedColor!
                 
-                if let crownNode = crownScene.rootNode.childNode(withName: "scene", recursively: true) {
-                    crownNode.position = SCNVector3(
-                        x: hitResult.worldTransform.columns.3.x,
-                        y: hitResult.worldTransform.columns.3.y,
-                        z: hitResult.worldTransform.columns.3.z
-                    )
-                    
-                    exampleArray.append(crownNode)
-                    
-                    sceneView.scene.rootNode.addChildNode(crownNode)
-                }
+                sphereNode.geometry?.materials = [sphereMaterial]
+                
+                sphereNode.position = SCNVector3(
+                    x: hitResult.worldTransform.columns.3.x,
+                    y: hitResult.worldTransform.columns.3.y,
+                    z: hitResult.worldTransform.columns.3.z
+                )
+                
+                exampleArray.append(sphereNode)
+                
+                sceneView.scene.rootNode.addChildNode(sphereNode)
             }else{
-                print("Clicked something else.")
+//                print("Clicked something else.")
             }
             
         }
     }
     
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        if let touch = touches.first {
-//            let touchLocation = touch.location(in: sceneView)
-//            
-//            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
-//            
-//            if let hitResult = results.first {
-//                print("Plane clicked.")
-//                let crownScene = SCNScene(named: "art.scnassets/crown.scn")!
-//                
-//                if let crownNode = crownScene.rootNode.childNode(withName: "scene", recursively: true) {
-//                    crownNode.position = SCNVector3(
-//                        x: hitResult.worldTransform.columns.3.x,
-//                        y: hitResult.worldTransform.columns.3.y,
-//                        z: hitResult.worldTransform.columns.3.z
-//                    )
-//                    
-//                    exampleArray.append(crownNode)
-//                    
-//                    sceneView.scene.rootNode.addChildNode(crownNode)
-//                }
-//            }else{
-//                print("Clicked something else.")
-//            }
-//            
-//        }
-//    }
-    
     func renderer(_ renderer: any SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if anchor is ARPlaneAnchor {
             let planeAnchor = anchor as! ARPlaneAnchor
             
-            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.y))
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
             
             let planeNode = SCNNode()
             
